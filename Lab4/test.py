c7c02cbe0048a3c1262cce621525f60d74cc6724
Victoria Gandz <vicsecrets100@gmail.com>
2025-04-01 17:56:26 +0300

    Add a file with the test for Lab4

---
+++ Lab4/test.py
@@ -0,0 +1,94 @@
+import unittest
+
+class Node:
+    def __init__(self, value, priority):
+        self.value = value
+        self.priority = priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+class HeapBasedPriorityQueue:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, value, priority):
+        self.heap.append(Node(value, priority))
+        self.heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if not self.heap:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap.pop().value
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+        return root.value
+
+    def peek(self):
+        if not self.heap:
+            return None
+        return self.heap[0].value
+
+    def heapify_up(self, index):
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.heap[index] < self.heap[parent_index]:
+                self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+                index = parent_index
+            else:
+                break
+
+    def heapify_down(self, index):
+        while True:
+            left_child_index = 2 * index + 1
+            right_child_index = 2 * index + 2
+            smallest_index = index
+
+            if left_child_index < len(self.heap) and self.heap[left_child_index] < self.heap[smallest_index]:
+                smallest_index = left_child_index
+
+            if right_child_index < len(self.heap) and self.heap[right_child_index] < self.heap[smallest_index]:
+                smallest_index = right_child_index
+
+            if smallest_index != index:
+                self.heap[index], self.heap[smallest_index] = self.heap[smallest_index], self.heap[index]
+                index = smallest_index
+            else:
+                break
+
+class TestHeapBasedPriorityQueue(unittest.TestCase):
+    def setUp(self):
+        self.pq = HeapBasedPriorityQueue()
+
+    def test_push_and_peek(self):
+        self.pq.push('n', 1)
+        self.pq.push('b', 4)
+        self.pq.push('c', 3)
+        self.pq.push('d', 2)
+        self.pq.push('e', 5)
+        self.pq.push('f', 6)
+        self.pq.push('hi', 0)
+        self.assertEqual(self.pq.peek(), 'hi')
+
+    def test_pop(self):
+        self.pq.push('n', 1)
+        self.pq.push('b', 4)
+        self.pq.push('c', 3)
+        self.pq.push('d', 2)
+        self.pq.push('hi', 0)
+        self.assertEqual(self.pq.pop(), 'hi')
+        self.assertEqual(self.pq.pop(), 'n')
+        self.assertEqual(self.pq.pop(), 'd')
+        self.assertEqual(self.pq.pop(), 'c')
+        self.assertEqual(self.pq.pop(), 'b')
+
+    def test_empty_queue(self):
+        self.assertIsNone(self.pq.peek())
+        self.assertIsNone(self.pq.pop())
+
+if __name__ == '__main__':
+    unittest.main()
